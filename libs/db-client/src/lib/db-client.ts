0ca3a41e9fb021c455eeb2919493d39aacf1e57a
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-10-13 15:09:06 +0300

    feat(db-client): ✨ add drizzle ORM config and schema files
    Add initial Drizzle ORM config and migration files for database client.
    Add new schema for authentication.
    Update db-client package and index for new ORM integration.

--- libs/db-client/src/lib/db-client.ts
+++ libs/db-client/src/lib/db-client.ts
@@ -2,7 +2,11 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: process.env.DATABASE_URL,
+  host: 'localhost',
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 export const dbClient = drizzle({ client: pool });
