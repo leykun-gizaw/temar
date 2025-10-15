82924ff8256e2d47680c3a7d496c1d0f565ce97e
Leykun-Gizaw <Leykungizaw@gmail.com>
2025-10-15 17:37:04 +0300

    refactor (db-client): ♻️ Update credential fetching to conform to docker standards.
    - Remove old docker compose and add a production config.

--- libs/db-client/src/lib/db-client.ts
+++ libs/db-client/src/lib/db-client.ts
@@ -2,8 +2,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  host: 'localhost',
+  host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
